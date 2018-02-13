927ffd6983e51ae8367f9d5ba58ae1f19256579e
Xindong Xu <xindong.xu@amlogic.com>
2018-02-13 14:41:31 +0800

    media: remove gpl [1/1]
    PD# 160531
    
    remove gpl
    
    Change-Id: I801d788cc59d92b708ae3ddc6c0c2beed30c8e49

--- amvdec/include/videodev2.h
+++ amvdec/include/videodev2.h
@@ -3,10 +3,6 @@
  *
  *  Copyright (C) 1999-2012 the contributors
  *
- *  This program is free software; you can redistribute it and/or modify
- *  it under the terms of the GNU General Public License as published by
- *  the Free Software Foundation; either version 2 of the License, or
- *  (at your option) any later version.
  *
  *  This program is distributed in the hope that it will be useful,
  *  but WITHOUT ANY WARRANTY; without even the implied warranty of
